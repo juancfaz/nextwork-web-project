646a30149974e3a126da7229b833cd4d3182ba3a
Juan Faz <jfazleal@icloud.com>
2025-08-06 22:11:28 -0700

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-sudo yum install tomcat9 -y
+sudo yum install tomcat -y
 sudo yum -y install httpd
-
-# Configuración del proxy inverso
-sudo cat << EOF > /etc/httpd/conf.d/tomcat.conf
+sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
-    ServerAdmin root@localhost
-    ServerName ec2-18-237-98-37.us-west-2.compute.amazonaws.com
-    
-    ProxyPreserveHost On
-    ProxyPass / http://localhost:8080/nextwork-web-project/
-    ProxyPassReverse / http://localhost:8080/nextwork-web-project/
-    
-    ErrorLog /var/log/httpd/nextwork-error.log
-    CustomLog /var/log/httpd/nextwork-access.log combined
+  ServerAdmin root@localhost
+  ServerName app.nextwork.com
+  DefaultType text/html
+  ProxyRequests off
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:8080/nextwork-web-project/
+  ProxyPassReverse / http://localhost:8080/nextwork-web-project/
 </VirtualHost>
 EOF
-
-# Habilitar módulos necesarios
-sudo systemctl enable httpd
-sudo systemctl enable tomcat9
